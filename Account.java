0cc4c52f57f74a2298058174c5fcc19f8d0629ff
Grispys <grispanimation@gmail.com>
2025-01-27 19:57:54 -0330

    created main menu in SimpleBankingApp.java using signin and signup methods from Account.java

--- Account.java
+++ Account.java
@@ -18,6 +18,14 @@ public class Account {
         System.out.print("Your balance: $" + this.balance);
     }
 
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
     public void deposit(double deposits){
         this.balance = this.balance + deposits;
         System.out.println("Deposit Successful. New Balance: " + this.balance);
@@ -34,13 +42,29 @@ public class Account {
     }
 
 
-    public static void signup(){
+    public static void signUp(){
         System.out.println("Please enter your username: ");
         String name = scanner.nextLine();
-        System.out.println("Please enter the seats COLUMN:");
+        System.out.println("Please your password: ");
         String pass= scanner.nextLine();
+        System.out.println("Account successfully Created");
         Account user = new Account(name, pass);
         users.add(user);
+    }
+
+    public static void signIn(){
+        System.out.print("Enter Username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+
+        Account tempUser = new Account(username, password);
+
+        for(Account users : users){
+            if(tempUser.getUsername().equals(username) && tempUser.getPassword().equals(password)){
+                System.out.println("Welcome, " + username);
+            }
+        }
     }
 
 
--- SimpleBankingApp.java
+++ SimpleBankingApp.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class SimpleBankingApp {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("\n--- Inertia Banking ---");
+            System.out.println("1. Sign in");
+            System.out.println("2. Sign up");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    Account.signIn();
+                    break;
+                case 2:
+                    Account.signUp();
+                    break;
+                case 3:
+                    System.out.println("Thank you, Goodbye!");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+
+
+    }
 }
