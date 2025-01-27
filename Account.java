4a276b2157df517f77e8f84fb5c7f7ab0dc7fc3b
Grispys <grispanimation@gmail.com>
2025-01-27 19:21:17 -0330

    created deposit and withdrawal methods

--- Account.java
+++ Account.java
@@ -14,6 +14,26 @@ public class Account {
         this.balance = 0.00;
     }
 
+    public void showBalance(){
+        System.out.print("Your balance: $" + this.balance);
+    }
+
+    public void deposit(double deposits){
+        this.balance = this.balance + deposits;
+        System.out.println("Deposit Successful. New Balance: " + this.balance);
+    }
+
+    public void withdraw(double withdrawal){
+        if(withdrawal > this.balance){
+            System.out.println("Withdrawal unsuccessful; Lack of funds. Your balance: $" + this.balance + ". Attempted Withdrawal: " + withdrawal);
+        }else{
+            this.balance = this.balance - withdrawal;
+            System.out.println("Withdrawal Successful. New Balance: " + this.balance);
+        }
+
+    }
+
+
     public static void signup(){
         System.out.println("Please enter your username: ");
         String name = scanner.nextLine();
